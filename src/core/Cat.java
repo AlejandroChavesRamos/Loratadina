d8770b122579cd93dffb412a06420379046327ab
Alejo Chaves Ramos <alejochaves05@gmail.com>
2025-03-21 15:54:30 -0500

    Commit feature2 gato

---
+++ src/core/Cat.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author alejo
+ */
+public class Cat {
+    private String Name;
+    private String Color;
+    private String Raza;
+}
